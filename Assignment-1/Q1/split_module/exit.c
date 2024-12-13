827828241f673e9705d3decbc238627c7bdd0bd5
86809-Gayatri-kaje <gayatrikaje17@gmail.com>
2024-12-13 16:08:29 +0530

    Assignment

--- Assignment-1/Q1/split_module/exit.c
+++
@@ -1,9 +0,0 @@
-#include<linux/init.h>
-#include<linux/module.h>
-
-static void vdg_exit(void)
-{
-    printk(KERN_INFO "exiting from the module with two files.....\n");
-}
-
-module_exit(vdg_exit);
